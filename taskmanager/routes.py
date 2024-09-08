b9b0d8b715c1518472869280b299e73907cfb875
ecolus <ecolus48@gmail.com>
2024-09-08 13:27:09 +0100

    Final changes

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -74,4 +74,4 @@ def delete_task(task_id):
     task = Task.query.get_or_404(task_id)
     db.session.delete(task)
     db.session.commit()
-    return redirect(url_for("add_task"))
+    return redirect(url_for("home"))
