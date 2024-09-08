f69c36ecb7717620e24b9e8ee3a2af1c2a288693
ecolus <ecolus48@gmail.com>
2024-09-08 13:19:05 +0100

    Completed walkthrough project

--- taskmanager/static/js/javascript.js
+++ taskmanager/static/js/javascript.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
 
-    var datepicker = document.querySelectorAll('.datepicker');
+    let datepicker = document.querySelectorAll('.datepicker');
     M.Datepicker.init(datepicker, {
       format: "dd mmmm, yyyy",
       i18n: {done: "Select"}
@@ -10,4 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
+
+    let collapsible = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsible)
   });
